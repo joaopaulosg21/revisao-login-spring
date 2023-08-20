3f85ef1d16c3340a0a5cfad0881d3b855a6e7681
joaopaulo <joaopaulosg17@gmail.com>
2023-08-20 09:28:01 -0300

    Commit inicial

---
+++ src/main/java/revisao/api/crudrevisao/CrudRevisaoApplication.java
@@ -0,0 +1,13 @@
+package revisao.api.crudrevisao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRevisaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRevisaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/revisao/api/crudrevisao/CrudRevisaoApplicationTests.java
@@ -0,0 +1,13 @@
+package revisao.api.crudrevisao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRevisaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
